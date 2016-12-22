cb23b5ead6c46daedfb07d9a38371dd72c2f4219
Emmanuel Maldonado <emaldonado@innomius.com>
2016-12-22 14:52:04 -0600

    Add css

--- app/app.js
+++ app/app.js
@@ -1,12 +1,11 @@
 import $ from "npm-zepto";
 import FirsFormView from "./first-form-view/first-form"
 
-
+/*entry point application*/
 export default class Application {
     init () {
       $(document).ready( () => { new FirsFormView(); });
     }
 }
-
 
 new Application().init();
